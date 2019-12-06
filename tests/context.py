77b93e79d8f5d61d37bbcc4fc06bf3142157f0d0
Alex Day <alex@alexday.me>
2019-12-06 15:20:45 +0000

    Removed tests dir bc it had nothing in it

--- tests/context.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
-
-from EmojiTranslation.Translators import *
-
-poset = PartOfSpeechEmojiTranslator("./data/emoji_joined.txt",
-                                    "/home/alex/Projects/Exhaustive-Chunking-Emoji-Translation/data/wiki_unigrams.bin", lambda x: x)
