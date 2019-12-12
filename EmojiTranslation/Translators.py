5083f0a157def512565afda093748c3fd0799412
Alex Day <alex@alexday.me>
2019-12-12 00:07:52 +0000

    Updated readme with better docs

--- EmojiTranslation/Translators.py
+++ EmojiTranslation/Translators.py
@@ -3,7 +3,7 @@ from typing import List, Tuple, Callable  # Datatypes for the function typing
 from abc import ABC, abstractmethod       # Abstract class helpers
 from string import punctuation            # Set of all punctuation
 from functools import lru_cache           # Annotation for storing func results
-
+from time import time                     # For timing
 
 # Import numpy for some helper functions
 import numpy as np
@@ -380,18 +380,16 @@ class ExhaustiveChunkingTranslation(AbstractEmojiTranslator):
 
 class PartOfSpeechEmojiTranslator(AbstractEmojiTranslator):
 
-    def pos_n_gram(self, sentence: str,
-                   keep_stop_words: bool=False) -> List[str]:
+    def pos_n_gram(self, sentence: str, keep_stop_words: bool=True) -> List[str]:
         """
-        Generate an n-gram based on the POS tagged dependency tree of the 
-        sentence that is "simplified" down according to a few assumptions that 
-        dictate a good sentence split. These assumptions are as follows:
-            1. If two words are leafs and on the same level with the same
-               parent they can be grouped as an n-gram
-            2. If there is a sequence of parent-child relationships
-               with only 1 child they can be grouped as one n-gram
+        Generate an n-gram based on the POS tagged dependency tree of the sentence that is "simplified" down according
+        to a few assumptions that dictate a good sentence split. These assumptions are as follows:
+            1. If two words are leafs and on the same level with the same parent they can be grouped as an n-gram
+            2. If there is a sequence of parent-child relationships with only 1 child they can be grouped as one
+               n-gram
+
+
         """
-        # Generate a naive list of stopwords
         stopword = "the in has be".split()
         pos_tagged_n_grams = []
 
@@ -404,10 +402,8 @@ class PartOfSpeechEmojiTranslator(AbstractEmojiTranslator):
 
             backlog.append((current_node.orth_, current_node.i))
             if current_node.n_lefts + current_node.n_rights > 1:
-                good_children = [child for child in current_node.children if len(
-                    list(child.children)) > 0]
-                bad_children = [(child.orth_, child.i)
-                                for child in current_node.children if child not in good_children]
+                good_children = [child for child in current_node.children if len(list(child.children)) > 0]
+                bad_children = [(child.orth_, child.i) for child in current_node.children if child not in good_children]
                 pos_tagged_n_grams.append(backlog)
                 pos_tagged_n_grams.append(bad_children)
                 return Tree(backlog, [Tree(bad_children, [])] + [to_nltk_tree(child) for child in good_children])
@@ -418,13 +414,12 @@ class PartOfSpeechEmojiTranslator(AbstractEmojiTranslator):
         def strip_nothing_unigrams(n_grams):
             return [n_gram for n_gram in n_grams if not (len(n_gram.split(" ")) == 1 and n_gram.split(" ")[0] in stopword)]
 
-    #     query = " ".join([word for word in sentence.split() if word not in stopword or keep_stop_words])
-        doc = self.nlp(sentence)
-        to_nltk_tree(list(doc.sents)[0].root)
+        query = " ".join([word for word in sentence.split() if word not in stopword or keep_stop_words])
+        doc = self.nlp(query)
+        to_nltk_tree(list(doc.sents)[0].root);
         # print(nltk_tree)
 
-        sort_inner = [sorted(nltk_child, key=lambda x: x[1])
-                      for nltk_child in pos_tagged_n_grams]
+        sort_inner = [sorted(nltk_child, key=lambda x: x[1]) for nltk_child in pos_tagged_n_grams]
 
         nltk_averages = []
         for nltk_child in sort_inner:
@@ -438,39 +433,48 @@ class PartOfSpeechEmojiTranslator(AbstractEmojiTranslator):
         for nltk_average in sorted_outer:
             n_grams.append(" ".join(word[0] for word in nltk_average[0]))
 
-        return n_grams
 
-    def summarize(self, sent: str, keep_stop_words: bool=False,
-                  lemma_func: Callable[[str], str]=lambda x: x) -> EmojiSummarizationResult:
+        if not keep_stop_words:
+            new_n_grams = []
+            for n_gram in n_grams:
+                new_n_gram = " ".join([word for word in word_tokenize(n_gram) if word not in stopword])
+    #             print(new_n_gram)
+                new_n_grams.append(new_n_gram)
+            return new_n_grams
+        else:
+            return n_grams
+
+
+
+    def summarize(self, sent:str, keep_stop_words:bool=True, lemma_func: Callable[[str], str]=lambda x: x) -> EmojiSummarizationResult:
         """
         Summarize a sentence using POS n-gram chunking
 
         Args:
             sent(str): Sentence to summarize
-            keep_stop_words(bool, Optional): Flag to keep the stop words when
-                                             cleaning the sentence and n-grams
-            lemma_func(Callable[[str], str], Optional): Function to use to
-                                                        lemmatize the sentence
+            keep_stop_words(bool, Optional): Flag to keep the stop words when cleaning the sentence and n-grams
+            lemma_func(Callable[[str], str], Optional): Function to use to lemmatize the sentence
 
         Rets:
             EmojiSummarizationResult: Result of the emoji summarization
         """
+        time_now = time()
 
         # Clean the sentence
-        sent = self.clean_sentence(
-            sent, keep_stop_words=True, lemma_func=lemma_func)
+        sent = self.clean_sentence(sent, keep_stop_words=True, lemma_func=lemma_func)
 
         # Get the n-grams using the part of speech tagging
         pos_n_grams = self.pos_n_gram(sent, keep_stop_words=keep_stop_words)
 
         # Clean the n_grams
-        n_grams = self.clean_n_gram(pos_n_grams)
+        n_grams = AbstractEmojiTranslator.clean_n_gram(pos_n_grams)
 
         # Create an Emoji Summarization Result
         esr = EmojiSummarizationResult()
 
         # Translate every n_gram in that n-gram sequence
         for n_gram in n_grams:
+            print(n_gram)
             # Get the closest emoji to the current n-gram
             emoji, similarity, desc = self.closest_emoji(n_gram)
 
@@ -479,6 +483,9 @@ class PartOfSpeechEmojiTranslator(AbstractEmojiTranslator):
             esr.emojis_n_grams.append(desc)
             esr.n_grams.append(n_gram)
             esr.uncertainty_scores.append(similarity)
+
+        # Stop the timer
+        esr.elapsed_time = time() - time_now
 
         # Return the summary
         return esr
--- README.md
+++ README.md
@@ -10,6 +10,32 @@ A naive chunking approach to summarizing a sentence to emoji translation using s
 - This project requires Python 3
 - Install dependencies with `pip install -r requirements.txt`
 - Install [sent2vec](https://github.com/epfml/sent2vec/tree/6b0eddec0c95e6e7f6f06582700305957311bfb9) according to the github repo
-- Download a [sent2vec model](https://github.com/epfml/sent2vec/tree/6b0eddec0c95e6e7f6f06582700305957311bfb9) to the models/directory
-- Download the [emoji2vec dataset](https://github.com/uclnlp/emoji2vec/blob/fd3dcb60a06b530c755ed1f1c8157d505b80e844/data/raw_training_data/emoji_joined.txt) to the data/ directory
-- `cd JupyterNotebooks && jupyter notebook` to start the jupyter notebook server to explore the algorithm
+- Download a [sent2vec model](https://github.com/epfml/sent2vec/tree/6b0eddec0c95e6e7f6f06582700305957311bfb9) to the `models/` directory
+- Download the [emoji2vec dataset](https://github.com/uclnlp/emoji2vec/blob/fd3dcb60a06b530c755ed1f1c8157d505b80e844/data/raw_training_data/emoji_joined.txt) to the `data/`` directory
+
+## Jupyter Notebook
+The `JupyterNotebook` 
+`cd JupyterNotebooks && jupyter notebook` to start the jupyter notebook server to explore the algorithm
+
+## Module
+The module contains the two separate algorithms in two separate classes. To use them simply import the `Translators` submodule from
+the `EmojiTranslation` module. In that submodule there are two classes of note `ExhaustiveChunkingTranslation` and `PartOfSpeechEmojiTranslator`.
+They both are instantiated with three paramters. An example of use is shown below
+```
+# Import the translation submodule 
+from EmojiTranslation import Translators
+
+# Paramererize the constructor arguments
+emoji_file = "./data/emoji_joined.txt" # Emoji keyword file from emoji2vec
+sent2vec_model = "../models/wiki_unigrams.bin" # sent2vec model
+nothing_lemma_func = lambda x: x # Lemmatization function that does nothing
+
+# Instantiate the exhaustive and part of speech translation algorithms
+exh = Translators.ExhaustiveChunkingTranslation(emoji_file, sent2vec_model, nothing_lemma_func)
+pos = Translators.PartOfSpeechEmojiTranslator(emoji_file, sent2vec_model, nothing_lemma_func)
+
+# Translate a sentence
+sent = "the dog ran fast"
+print(exh.summarize(sent))
+print(pos.summarize(sent))
+```
